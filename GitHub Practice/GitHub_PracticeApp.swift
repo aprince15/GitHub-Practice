02e3b96756c7bf68d557b27b7fabc6e93bd8fcbe
Anika Prince <anika.prince15@gmail.com>
2024-07-26 11:53:19 -0400

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by Anika P on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
